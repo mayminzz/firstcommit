a358129c9181a71ac2da38c74751ec7eb62ef448
mayminzz <mayminzz@naver.com>
2023-10-20 09:49:10 +0900

    20231019home

---
+++ 02_Project/script.js
@@ -0,0 +1,14 @@
+// $(function () {
+//   $(window).scroll(function () {
+//     if ($(window).scrollTop() > 40) {
+//       $(".trigger").addClass("active");
+//     } else {
+//       $(".gnb").removeClass("active")
+//     }
+//   });
+// });
+
+$("label[for=tab2]").click(function() {
+  $(this).toggleClass("active");
+  $(this)
+})
