67fcca38af7ff33f4b45088ad217e63e72cc408b
mayminzz <mayminzz@naver.com>
2023-10-23 02:03:04 +0900

    20231023

--- 02_Project/script.js
+++ 02_Project/script.js
@@ -1,14 +1,32 @@
-// $(function () {
-//   $(window).scroll(function () {
-//     if ($(window).scrollTop() > 40) {
-//       $(".trigger").addClass("active");
-//     } else {
-//       $(".gnb").removeClass("active")
-//     }
-//   });
-// });
 
-$("label[for=tab2]").click(function() {
-  $(this).toggleClass("active");
-  $(this)
-})
+const gnb = document.querySelector(".gnb");
+const menu = document.querySelector(".menu");
+const trigger = document.querySelector(".trigger");
+
+trigger.onclick = () => {
+  trigger.classList.toggle("active");
+  menu.classList.toggle("show");
+}
+
+const img2 = document.querySelector(".img_2");
+const text = document.querySelector(".text");
+
+text.onclick = () => {
+  img2.classList.toggle("overImg");
+};
+
+// img1.addEventListener("click", ()=> {
+//   img2.classList.toggle("overImg")
+// })
+
+
+
+const myName = document.querySelector("#name");
+
+myName.onclick = () => {
+  if(myName.innerText == "권민주") {
+    myName.innerText = "KWON MIN JOO"
+  } else if(myName.innerText == "KWON MIN JOO") {
+    myName.innerText = "권민주"
+  }
+}
