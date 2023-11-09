5eadbcb520d0d35d412ccb6e9aa7670a37ffbb6d
mayminzz <mayminzz@naver.com>
2023-11-10 01:30:43 +0900

    20231108home

--- group_project/script/qa-script.js
+++ group_project/script/qa-script.js
@@ -1,22 +1,24 @@
 // GiftTop
+const giftTop = document.querySelector(".gift_top")
 window.addEventListener("load", () => {
-  document.querySelector(
-    ".gift_top"
-  ).innerHTML = `<h4>전 구매 무료상품 및 선물포장과 단독 기프트 메시지 카드 혜택을 즐겨보세요.</h4>`;
+  giftTop.innerHTML = `<h4>전 구매 무료상품 및 선물포장과 단독 기프트 메시지 카드 혜택을 즐겨보세요.</h4>`;
 });
 
-// left gnb
+//====== left gnb =======
 const headerUrl = "../json/header.json";
 fetch(headerUrl)
   .then((response) => response.json())
   .then((json) => {
     let leftOutput = "";
     json.leftNav.forEach((list) => {
-      leftOutput += `<a href="#">${list.category}</a>`;
-      console.log(leftOutput);
+      leftOutput += `<li><a href="#">${list.category}</a></li>`;
     });
-    document.querySelector(".gnb").innerHTML = leftOutput;
+    const ulGnb = document.createElement("ul");
+    const divGnb = document.querySelector(".gnb");
+    divGnb.appendChild(ulGnb);
+    ulGnb.innerHTML = leftOutput;
 
+// ====== right Gnb ======
     let rightOutput = "";
 
     json.rightNav.forEach((list) => {
@@ -26,10 +28,18 @@ fetch(headerUrl)
     const gnbRight = document.querySelector(".gnb_right");
     gnbRight.innerHTML = rightOutput;
 
-    const findIcon = document.createElement("a");
-    findIcon.classList.add("finder");
-    json.rightNav.unshift(findIcon);
+    // const findIcon = document.createElement("a");
+    // findIcon.classList.add("finder");
+    // json.rightNav.unshift(findIcon);
 
+// ======== mobole Gnb =========
+
+    const modalGnb = document.querySelector("#mobile_gnb_list"); 
+    const modalUl = document.createElement("ul");
+    modalGnb.appendChild(modalUl);
+    modalUl.innerHTML = leftOutput;
+
+// ======== toggle =========
     const newDiv = document.createElement("div");
     const span1 = document.createElement("span");
     const span2 = document.createElement("span");
@@ -39,25 +49,41 @@ fetch(headerUrl)
     newDiv.appendChild(span1);
     newDiv.appendChild(span2);
     json.rightNav.push(newDiv);
+    
+    newDiv.addEventListener("click", () => {
+      newDiv.classList.toggle("active");
+      giftTop.style.display = "none"
+      modalGnb.classList.toggle("showModalGnb");
+    });
   })
-  .catch((err) => console.log(err));
-
-// toggle
-const newDiv = document.createElement("div");
-newDiv.classList.add("toggle");
-
-newDiv.addEventListener("click", () => {
-  newDiv.classList.toggle("active");
-});
+  .catch((err) => console.log(err)); 
 
 // 제출하기 버튼 이벤트
 const submitBtn = document.querySelector("#submitbtn");
-const userName = document.querySelector("#name");
+let userName = document.querySelector("#name");
+let userEmail = document.querySelector("#email");
+
 submitBtn.addEventListener("click", () => {
-  alert(`${userName.value}님, 문의하신 내용이 제출되었습니다. 감사합니다!`);
+  // try{
+  //   if(userName.value !== "" && userEmail.value !== "") {
+  //     throw `${userName.value}님, 문의하신 내용이 제출되었습니다. 감사합니다!`
+  //   } else {
+  //     throw "필수 입력 항목을 작성해주세요!"
+  //   }
+  // } catch(err) {
+  //   alert(err)
+  // } finally {
+  //     userName = "";
+  //     userEmail = "";
+  // }
+  if(userName.value !== "" &&userEmail.value !== "") {
+    alert(`${userName.value}님, 문의하신 내용이 제출되었습니다. 감사합니다!`);
+  } else {
+    alert("필수입력 항목을 작성해주세요!")
+  }
 });
 
-//작은 문의사항 창
+//오른 쪽 작은 문의사항 창
 const btn = document.querySelector("#query");
 const miniBox = document.querySelector(".mini_ask");
 
@@ -65,8 +91,9 @@ btn.addEventListener("click", () => {
   miniBox.classList.toggle("show");
 });
 
-const footerUrl = "../json/footer.json";
 // footer
+const footerUrl = "../json/footer.json";
+
 fetch(footerUrl)
   .then((response) => response.json())
   .then((json) => {
