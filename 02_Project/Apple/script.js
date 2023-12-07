0b3315241cd1b8b9e423e98fe5e8d6bf2f8d573a
mayminzz <mayminzz@naver.com>
2023-12-07 16:30:58 +0900

    20231207

--- 02_Project/Apple/script.js
+++
@@ -1,8 +0,0 @@
-const trigger = document.querySelector(".trigger");
-const container = document.querySelector(".container");
-
-trigger.addEventListener("click", () => {
-  trigger.classList.toggle("active");
-  container.classList.toggle("removeContainer")
-});
-
