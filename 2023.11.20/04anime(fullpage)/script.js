a785cd2affac452278195253cdecd591f00f3a4d
mayminzz <mayminzz@naver.com>
2023-12-22 17:48:23 +0900

    20231222

--- 2023.11.20/04anime(fullpage)/script.js
+++ 2023.11.20/04anime(fullpage)/script.js
@@ -1,29 +1,30 @@
-const Body = document.querySelector("body");
-const Nav_btn = document.querySelector("#nav_icon");
+const Body = document.querySelector('body');
+const Nav_btn = document.querySelector('#nav_icon');
 
 // section2 slider
-const s_Icons = document.querySelectorAll(".slide_icons li");
-const s_Left = document.querySelector(".slide_btn.left");
-const s_Right = document.querySelector(".slide_btn.right");
-const Bg = document.querySelector("#sec2");
-const s_Slider = document.querySelectorAll(".sec2_slider li");
+const s_Icons = document.querySelectorAll('.slide_icons li');
+const s_Left = document.querySelector('.slide_btn.left');
+const s_Right = document.querySelector('.slide_btn.right');
+const Bg = document.querySelector('#sec2');
+const s_Slider = document.querySelectorAll('.sec2_slider li');
 
 const s_reset = () => {
   s_Slider.forEach((elem, idx) => {
-    s_Slider[idx].classList.remove("on");
-    s_Icons[idx].classList.remove("on");
+    s_Slider[idx].classList.remove('on');
+    s_Icons[idx].classList.remove('active');
   });
 };
+
 s_Icons.forEach((li) => {
-  li.addEventListener("click", (e) => {
+  li.addEventListener('click', (e) => {
     let target = e.target.dataset.index;
     console.log(target);
     s_reset();
-    if (li.tagName === "LI") {
+    if (li.tagName === 'LI') {
       for (let i = 0; i < s_Icons.length; i++) {
         if (target == i) {
-          s_Slider[i].classList.add("on");
-          s_Icons[i].classList.add("active");
+          s_Slider[i].classList.add('on');
+          s_Icons[i].classList.add('active');
           Bg.style.backgroundImage = `url(./portfolio/img/sec2_bg_${[i]}.png)`;
         }
       }
@@ -33,122 +34,123 @@ s_Icons.forEach((li) => {
 
 const next = (e) => {
   e.preventDefault();
-  let crtSlide = document.querySelector(".sec2_slider li.on");
+  let crtSlide = document.querySelector('.sec2_slider li.on');
   let i = crtSlide.dataset.index;
   s_reset();
   i++;
   if (i >= s_Slider.length) {
     i = 0;
   }
-  s_Slider[i].classList.add("on");
-  s_Icons[i].classList.add("active");
+  s_Slider[i].classList.add('on');
+  s_Icons[i].classList.add('active');
   Bg.style.backgroundImage = `url(./portfolio/img/sec2_bg_${[i]}.png)`;
 };
 
-s_Right.addEventListener("click", next);
+s_Right.addEventListener('click', next);
+
 const prev = (e) => {
   e.preventDefault();
-  let crtSlide = document.querySelector(".sec2_slider li.on");
+  let crtSlide = document.querySelector('.sec2_slider li.on');
   let i = crtSlide.dataset.index;
   s_reset();
   i--;
-  if (i >= s_Slider.length) {
-    i = 0;
+  if (i < 0) {
+    i = s_Slider.length - 1;
   }
-  s_Slider[i].classList.add("on");
-  s_Icons[i].classList.add("active");
+  s_Slider[i].classList.add('on');
+  s_Icons[i].classList.add('active');
   Bg.style.backgroundImage = `url(./portfolio/img/sec2_bg_${[i]}.png)`;
 };
 
-s_Right.addEventListener("click", prev);
+s_Left.addEventListener('click', prev);
 
-Nav_btn.addEventListener("click", () => {
-  Body.classList.toggle("nav_active");
+Nav_btn.addEventListener('click', () => {
+  Body.classList.toggle('nav_active');
 });
 
 const sec0 = () => {
   anime({
-    targets: ".svg1 path",
+    targets: '.svg1 path',
     strokeDashoffset: [anime.setDashoffset, 0],
-    easing: "easeInOutSine",
+    easing: 'easeInOutSine',
     duration: 4000,
     delay: function (el, i) {
       return i * 400;
     },
-    direction: "alternate",
+    direction: 'alternate',
     loop: true,
   });
 };
 const sec1 = () => {
   let tl = anime.timeline({
-    easing: "linear",
+    easing: 'linear',
     duration: 500,
   });
   tl.add({
-    targets: ".g01",
-    height: "80%",
+    targets: '.g01',
+    height: '80%',
   })
     .add({
-      targets: ".g02",
-      height: "70%",
+      targets: '.g02',
+      height: '70%',
     })
     .add({
-      targets: ".g03",
-      height: "80%",
+      targets: '.g03',
+      height: '80%',
     })
     .add({
-      targets: ".g04",
-      height: "85%",
+      targets: '.g04',
+      height: '85%',
     });
 };
 const sec1_reset = () => {
   anime({
-    targets: ".gage_in",
-    height: "0%",
+    targets: '.gage_in',
+    height: '0%',
   });
 };
 const sec2 = () => {
   let tl = anime.timeline({
     duration: 500,
-    easing: "linear",
+    easing: 'linear',
   });
   tl.add({
-    targets: "#sec2 h1",
+    targets: '#sec2 h1',
     opacity: 1,
-    translateY50,
+    translateY: 50,
   }).add({
-    targets: "slider_wrap",
+    targets: '.slider_wrap',
     opacity: 1,
     translateY: 50,
   });
 };
 const sec3 = () => {
   const d0 =
-    "M453 100C283.5 11.5 184 0.499989 0.5 0.5V772H1921V63C1753.5 144 1381.5 288 1146 288C825 288 726.67 242.89 453 100Z";
+    'M453 100C283.5 11.5 184 0.499989 0.5 0.5V772H1921V63C1753.5 144 1381.5 288 1146 288C825 288 726.67 242.89 453 100Z';
   const d1 =
-    "M570 10C308.5 -26.5 135 62 0.5 95V730H1921V95C1753.5 176 1579.5 209 1344 209C1028.11 209 875.763 52.6782 570 10Z";
+    'M570 10C308.5 -26.5 135 62 0.5 95V730H1921V95C1753.5 176 1579.5 209 1344 209C1028.11 209 875.763 52.6782 570 10Z';
   const d2 =
-    "M585.5 276C367.959 243.273 245 160.5 0.5 20V729H1921V20C1693 -24 1501 6.78688 1312 147.287C1070.85 326.558 758.5 302.027 585.5 276Z";
+    'M585.5 276C367.959 243.273 245 160.5 0.5 20V729H1921V20C1693 -24 1501 6.78688 1312 147.287C1070.85 326.558 758.5 302.027 585.5 276Z';
 
   anime({
-    targets: ".sec3_svg path",
+    targets: '.sec3_svg path',
     d: [{ value: d0 }, { value: d1 }, { value: d2 }],
-    fill: [{ value: "#667eea" }, { value: "#764ba2" }, { value: "#66a6ff" }],
+    fill: [{ value: '#667eea' }, { value: '#764ba2' }, { value: '#66a6ff' }],
     duration: 3000,
-    easing: "easeInOutQuart",
+    easing: 'easeInOutQuart',
     loop: true,
-    direction: "alternate",
+    direction: 'alternate',
   });
 
   anime({
-    targets: "#sec3 h1 span",
+    targets: '#sec3 h1 span',
     delay: anime.stagger(100),
     opacity: 1,
     translateX: 100,
-    easing: "easeOutSine",
+    easing: 'easeOutSine',
   });
 };
-const staggerWrap = document.querySelector("#sec4 .img_wrap");
+const staggerWrap = document.querySelector('#sec4 .img_wrap');
 const fragment = document.createDocumentFragment();
 const grid = [20, 20];
 const col = grid[0];
@@ -156,18 +158,18 @@ const row = grid[1];
 
 const allElem = col * row;
 for (let i = 0; i < allElem; i++) {
-  const div = document.createElement("div");
+  const div = document.createElement('div');
   fragment.appendChild(div);
-  div.className = "tail";
+  div.className = 'tail';
 }
 
 staggerWrap.appendChild(fragment);
 
 const sec4 = () => {
   const stageAni = anime.timeline({
-    targets: ".tail",
-    easing: "easeInBack",
-    delay: anime.stagger(10, { from: "last" }),
+    targets: '.tail',
+    easing: 'easeInBack',
+    delay: anime.stagger(10, { from: 'last' }),
     duration: 2000,
     endDelay: 1000,
     loop: false,
@@ -175,7 +177,7 @@ const sec4 = () => {
   });
   stageAni
     .add({
-      targets: "#sec4 h1 img",
+      targets: '#sec4 h1 img',
       opacity: 0,
       duration: 500,
     })
@@ -186,15 +188,15 @@ const sec4 = () => {
       translateY: function () {
         return anime.random(-500, 500);
       },
-      delay: anime.stagger(200, { grid: grid, from: "last" }),
+      delay: anime.stagger(200, { grid: grid, from: 'last' }),
       scale: 0.2,
-      background: "#fff",
-      borderRadius: "50%",
+      background: '#fff',
+      borderRadius: '50%',
     })
     .add({
       targets: staggerWrap,
       rotate: 360,
-      easing: "linear",
+      easing: 'linear',
       duration: 4000,
       scale: 0.5,
     })
@@ -206,72 +208,72 @@ const sec4 = () => {
     .add({
       translateX: 0,
       translateY: 0,
-      delay: anime.stagger(100, { grid: grid, from: "center" }),
+      delay: anime.stagger(100, { grid: grid, from: 'center' }),
       duration: 3000,
       scale: 0.8,
-      background: "#2af598",
+      background: '#2af598',
     })
     .add({
       scale: 0.5,
       duration: 500,
       rotate: 60,
-      borderRadius: "0%",
-      delay: anime.stagger(100, { grid: grid, from: "center" }),
+      borderRadius: '0%',
+      delay: anime.stagger(100, { grid: grid, from: 'center' }),
     })
     .add({
       scale: 0.8,
       duration: 500,
       rotate: -60,
-      borderRadius: "50%",
-      background: "#fff",
-      delay: anime.stagger(100, { grid: grid, from: "center" }),
+      borderRadius: '50%',
+      background: '#fff',
+      delay: anime.stagger(100, { grid: grid, from: 'center' }),
     })
     .add({
       scaleX: 0.1,
       scaleY: 1,
       duration: 500,
       rotate: 120,
-      borderRadius: "0%",
-      background: "#2af598",
-      delay: anime.stagger(100, { grid: grid, from: "center" }),
+      borderRadius: '0%',
+      background: '#2af598',
+      delay: anime.stagger(100, { grid: grid, from: 'center' }),
     })
     .add({
       duration: 500,
       rotate: 0,
-      delay: anime.stagger(100, { grid: grid, from: "center" }),
+      delay: anime.stagger(100, { grid: grid, from: 'center' }),
     })
     .add({
       scaleX: 1,
       duration: 500,
-      delay: anime.stagger(100, { grid: grid, from: "center" }),
+      delay: anime.stagger(100, { grid: grid, from: 'center' }),
     })
     .add({
       scale: 1,
       duration: 800,
-      background: "#009efd",
-      delay: anime.stagger(100, { grid: grid, from: "center" }),
+      background: '#009efd',
+      delay: anime.stagger(100, { grid: grid, from: 'center' }),
     })
     .add({
-      targets: "#sec4 h1 img",
+      targets: '#sec4 h1 img',
       opacity: 1,
       duration: 500,
     });
 
   // stageAni.play();
 
-  staggerWrap.addEventListener("click", () => {
+  staggerWrap.addEventListener('click', () => {
     stageAni.play();
   });
 };
 
 sec4();
 
-new fullpage("#fullpage", {
+new fullpage('#fullpage', {
   //options here
   autoScrolling: true,
   scrollHorizontally: true,
   navigation: true,
-  anchors: ["Num0", "Num1", "Num2", "Num3", "Num4"],
+  anchors: ['Num0', 'Num1', 'Num2', 'Num3', 'Num4'],
   afterLoad: (old_elem, new_elem, direction) => {
     if (new_elem.index === 0) {
       sec0();
