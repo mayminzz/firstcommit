2a0ce0bec307e90e17fdb11e1ef356275b430ff8
mayminzz <mayminzz@naver.com>
2024-01-30 21:54:55 +0900

    2024.01.30

--- 2024.01.30/02js/script.js
+++ 2024.01.30/02js/script.js
@@ -5,8 +5,8 @@
 // > 버튼에 클릭 이벤트 부여 = addEventListener() 적용
 
 // 2. 사용자가 버튼을 클릭하면, 컴퓨터에게 커튼이 클릭되었다는 사실을 알려준다!
-// > 웹 브라우저 화면에 출력할 요소를 띄워줘야한다. 
-// > 띄워줄 화면(*container)을 html에서 찾아와서 어떤 변숭 담아야 한다. 
+// > 웹 브라우저 화면에 출력할 요소를 띄워줘야한다.
+// > 띄워줄 화면(*container)을 html에서 찾아와서 어떤 변숭 담아야 한다.
 
 // 3. 컴퓨터는 버튼이 클릭되었다는 사실을 인지하면, 방금 display : none 을 설정했던 container라는 클래스에 다시 display : flex를 적용시킨다.
 // > JS에서 CSS스타일 적용시키는 방법
@@ -20,11 +20,22 @@
 const openBtn = document.querySelector(".open");
 const container = document.querySelector(".container");
 const closeBtn = document.querySelector(".close");
-console.log(container);
 
-openBtn.addEventListener("click", () => {
-  container.classList.add("show");
-});
-closeBtn.addEventListener("click", () => {
-  container.classList.remove("show");
-});
+// openBtn.addEventListener("click", () => {
+//   container.classList.add("show");
+// });
+
+// closeBtn.addEventListener("click", () => {
+//   container.classList.remove("show");
+// });
+const display = () => {
+  container.style.display = "flex";
+  openBtn.style.display = "none";
+};
+const disappear = () => {
+  container.style.display = "none";
+  openBtn.style.display = "block";
+};
+
+openBtn.addEventListener("click", display);
+closeBtn.addEventListener("click", disappear);
