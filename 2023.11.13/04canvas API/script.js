a8893ffbc994c87ccb8492ecee89995a55f44e62
mayminzz <mayminzz@naver.com>
2024-05-02 16:55:08 +0900

    2024.05.01

--- 2023.11.13/04canvas API/script.js
+++ 2023.11.13/04canvas API/script.js
@@ -21,16 +21,12 @@ ctx.lineTo(50, 150);
 ctx.closePath();
 ctx.stroke();
 
-// ctx.beginPath();
-// ctx.moveTo(150, 100);
-// ctx.lineTo(250, 50);
-// ctx.lineTo(250, 150);
-// ctx.closePath();
-// // 기본적으로 설정하지 않으면 검정색
-// // ctx.fill()
-// ctx.fillStyle = "rgb(0, 200, 0)";
-// ctx.fill();
-
-
-
-
+ctx.beginPath();
+ctx.moveTo(150, 100);
+ctx.lineTo(250, 50);
+ctx.lineTo(250, 150);
+ctx.closePath();
+// 기본적으로 설정하지 않으면 검정색
+// ctx.fill()
+ctx.fillStyle = "rgb(0, 200, 0)";
+ctx.fill();
